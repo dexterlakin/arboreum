00b8e7f509991ad70be5f3ff2349e24d7d1ee909
spider-g <guilherme@sage.art>
2022-01-14 22:03:10 -0300

    wallet connect

--- src/App.js
+++ src/App.js
@@ -1,19 +1,88 @@
-import "./App.css";
+import { Web3ReactProvider } from "@web3-react/core";
+import Web3Provider from "web3";
+import { useWeb3React } from "@web3-react/core";
+import { InjectedConnector } from "@web3-react/injected-connector";
+import { ToastContainer, toast } from "react-toastify";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p class="font-semibold">A r b o r e u m</p>
-        <br/>
-        <img src="tree.png" width="300" height="300" alt="" />
-        <br />
-        <button class="bg-white hover:bg-gray-100 text-gray-800 text-base font-semibold py-2 px-4 border border-gray-400 rounded shadow">
-          connect your wallet
-        </button>
-      </header>
-    </div>
-  );
+import "./App.css";
+import "react-toastify/dist/ReactToastify.css";
+
+const injected = new InjectedConnector({ supportedChainIds: [4] });
+
+function getLibrary(provider) {
+  return new Web3Provider(provider);
 }
 
-export default App;
+const App = () => {
+  const { active, account, library, connector, activate, deactivate } = useWeb3React();
+
+  async function handleMintNftClick() {
+    toast.info("You wish!");
+  }
+
+  async function handleConnectWalletClick() {
+    try {
+      await activate(injected);
+    } catch (ex) {
+      console.log(ex);
+    }
+  }
+
+  async function handleDisconnectWalletClick() {
+    try {
+      deactivate();
+    } catch (ex) {
+      console.log(ex);
+    }
+  }
+
+  return (
+    <div className="App">
+      {/* Bootstrap CSS should be at beginning of page */}
+      <link
+        href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
+        rel="stylesheet"
+        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
+        crossorigin="anonymous"
+      ></link>
+      <header className="App-header">
+        <ToastContainer />
+        <h1 className="display-6">A r b o r e u m</h1>
+        <img className="mt-3 mb-5" src="tree.png" width="275" height="275" alt="" />
+        {active ? (
+          <>
+            <small>
+              connected with <b>{account}</b>
+            </small>
+            <br />
+            <button type="button" onClick={handleMintNftClick} className="btn btn-outline-dark">
+              mint nft
+            </button>
+            <br />
+            <button type="button" onClick={handleDisconnectWalletClick} className="btn btn-outline-dark">
+              disconnect
+            </button>
+          </>
+        ) : (
+          <button type="button" onClick={handleConnectWalletClick} className="btn btn-outline-dark">
+            connect your wallet
+          </button>
+        )}
+      </header>
+      {/* Bootstrap JS should be at end of page */}
+      <script
+        src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
+        crossorigin="anonymous"
+      ></script>
+    </div>
+  );
+};
+
+export default function () {
+  return (
+    <Web3ReactProvider getLibrary={getLibrary}>
+      <App />
+    </Web3ReactProvider>
+  );
+}
